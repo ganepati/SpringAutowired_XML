069bd8c6e31c8171221175043ae207142b5d4d64
Ganapati <Ganapati@Ganapati-PC>
2018-11-30 08:19:01 +0530

    First Commit

---
+++ Autowired_XML/src/beans/Car.java
@@ -0,0 +1,18 @@
+package beans;
+
+public class Car {
+	
+	private Engine engine;
+	
+	public void setEngine(Engine engine) {
+		this.engine = engine;
+	}
+	public Engine getEngine() {
+		return engine;
+	}
+
+	public void printData(){
+		System.out.println("Model Year :  "+engine.getModelYear() );
+	}
+
+}
